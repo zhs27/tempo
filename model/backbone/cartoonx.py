e0da965974719e0a697e1ee2c33717706d7de19b
Haosheng Zhang <zhanghaosheng2000@163.com>
2024-05-01 14:24:04 -0400

    test

--- model/backbone/cartoonx.py
+++ model/backbone/cartoonx.py
@@ -150,7 +150,7 @@ class CartoonX:
                 for n in m: 
                     l1waveletcoefs += n.abs().sum()
             print(m_yl[0].size())
-            print(m_yh[0][0])
+            print(m_yh[0][0].size())
             l1waveletcoefs /= num_mask
             
             # Log losses
