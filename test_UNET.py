acd9213de68a0c70250462b0ef597f359a4cc865
Haosheng Zhang <zhanghaosheng2000@163.com>
2024-05-02 15:26:47 -0400

    1

--- test_UNET.py
+++ test_UNET.py
@@ -208,7 +208,8 @@ def main(cfg):
         print(cartoonx.size())
         for m,n in zip(x, cartoonx):
             unetpred = s(m)
-            print(unetpred)
+            print(torch.eq(unetpred, n))
+
         for j in range(6):
             picname1 = str(j) + '.png'
             picname2 = 'cartoonx' + str(j) + '.png'
