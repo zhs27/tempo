fc0f5d624b9cdc4f7a76ccb36a1b59dfa6e6c12a
Haosheng Zhang <zhanghaosheng2000@163.com>
2024-05-07 23:50:07 -0400

    1

--- test_UNET.py
+++ test_UNET.py
@@ -184,8 +184,8 @@ def main(cfg):
 
     s = UNet(n_channels=1, n_classes=1)
     s = s.to(cfg.device)
-    unetopt = optim.RMSprop(s.parameters(),
-                              lr=1e-1, weight_decay=1e-8, momentum=0.999, foreach=True)
+    unetopt = optim.Adam(s.parameters(),
+                              lr=1e-1, weight_decay=1e-5)
     criterion = nn.CrossEntropyLoss()
     s.train()
     
