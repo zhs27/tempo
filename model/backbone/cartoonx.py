10d58970194139fc0080c21f83cf998e25b5003e
Haosheng Zhang <zhanghaosheng2000@163.com>
2024-05-20 17:48:58 -0400

    1

--- model/backbone/cartoonx.py
+++ model/backbone/cartoonx.py
@@ -83,7 +83,6 @@ class CartoonX:
             yh.append(y2)
             m_yl.append(m_y1)
             m_yh.append(m_y2)
-        print(m_yl[0].size(), m_yh[0][0].size())
         
         # compute obfuscation strategy
 
