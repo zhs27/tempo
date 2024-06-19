91359308783b88e3f19de11441e2e9907626a148
Haosheng Zhang <zhanghaosheng2000@163.com>
2024-06-18 21:21:33 -0400

    test

--- main3.py
+++ main3.py
@@ -37,7 +37,7 @@ def get_arg():
     cfg.add_argument('--lr_sch',default=False)
     cfg.add_argument('--data_aug',default=True)
     cfg.add_argument('--dataset',default='ModeNet40C',choices=['ScanObjectNN','ModeNet40','ModeNet40C'])
-    cfg.add_argument('--pretrain_epochs',default=20,type=int)
+    cfg.add_argument('--pretrain_epochs',default=1,type=int)
 
 
     # ======== few shot cfg =============#
@@ -174,7 +174,7 @@ def train_model(modelQ,modelQh, train_loader,val_loader,cfg, upfreq = 5):
 
     def train_one_epoch(m1, m2, optimizer, xtocartoonx):
         bar=tqdm(train_loader,ncols=100,unit='batch',leave=False)
-        epsum=run_one_epoch(m1,m2,bar,'train',loss_func=loss_func,xtocartoonx = xtocartoonx, optimizerQ=optimizer, mixup = True)
+        epsum=run_one_epoch(m1,m2,bar,'train',loss_func=loss_func,xtocartoonx = xtocartoonx, optimizerQ=optimizer, mixup = False)
         summary={"loss/train":np.mean(epsum['loss'])}
         return summary
         
